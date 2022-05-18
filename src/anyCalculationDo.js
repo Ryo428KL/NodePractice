855632d5cbbf3f1c33846e63bcd20edcf49cedea
RYOMATSUI <ryo.428.matsui@gmail.com>
2022-05-19 01:48:07 +0900

    電卓基礎 エラーパターン追加

--- src/anyCalculationDo.js
+++ src/anyCalculationDo.js
@@ -7,6 +7,9 @@ const Fomula1 = '(Max+MIN)/2';
 const Fomula2 = 'AVE/Max * 100';
 const Fomula3 = 'AVE/Min * 100';
 const Fomula4 = '(AVE/Min - AVE/Max) * 100'
+const FomulaErr1 = 'aaa/Min * 100'
+const FomulaErr2 = 'ABE/Min * 100'
+const FomulaErr3 = 'AVE/Minn * 100'
 
 // 実行
 const res1_1 = calc.anyCalc(Nums1, Fomula1);
@@ -22,7 +25,13 @@ const res3_2 = calc.anyCalc(Nums3, Fomula2);
 const res3_3 = calc.anyCalc(Nums3, Fomula3);
 const res3_4 = calc.anyCalc(Nums3, Fomula4);
 
+const res3_er1 = calc.anyCalc(Nums3, FomulaErr1);
+const res3_er2 = calc.anyCalc(Nums3, FomulaErr2);
+const res3_er3 = calc.anyCalc(Nums3, FomulaErr3);
+
 console.log('res1_1:',res1_1,' res1_2:',res1_2,' res1_3:',res1_3,' res1_4:',res1_4);
 console.log('res2_1:',res2_1,' res2_2:',res2_2,' res2_3:',res2_3,' res2_4:',res2_4);
 console.log('res3_1:',res3_1,' res3_2:',res3_2,' res3_3:',res3_3,' res3_4:',res3_4);
+console.log('res3_er1:', res3_er1, ' res3_er2:', res3_er2, ' res3_er3:', res3_er3);
+
 
